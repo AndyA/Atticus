61fdaebe61581f14c19c11fa32468712dadd242b
Andy Armstrong <andy@hexten.net>
2017-03-21 21:34:49 +0000

    Basic setup.

--- app/bin/app.pl
+++ app/bin/app.pl
@@ -1,4 +1,7 @@
 #!/usr/bin/env perl
+
 use Dancer;
-use Atticus;
+
+use Atticus::Site;
+
 dance;
--- app/lib/Atticus/Site.pm
+++ app/lib/Atticus/Site.pm
@@ -1,10 +1,13 @@
-package Atticus;
+package Atticus::Site;
+
+use v5.10;
+
 use Dancer ':syntax';
 
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
+  template 'index';
 };
 
 true;
